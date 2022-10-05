3b16d360ba893699d979fbd655aaac942f7f9e57
Dhivyajoel <chat2joe86@gmail.com>
2022-10-05 19:47:42 +0530

    TestNg_Example

---
+++ CardTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class CardTest extends BaseClass{
+	
+	@Test
+	public void CCTest() {
+		
+		System.out.println("Inside CC Test");
+		
+	}
+
+}
+
+
---
+++ LoanTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class LoanTest extends BaseClass{
+	
+	
+	
+	@Test
+	public void HomeLoanTest() {
+		
+		System.out.println("Inside Home Loan Test");
+	}
+	
+	@Test
+	public void PersonalLoanTest() {
+		
+		System.out.println("Inside Personal Loan Test");
+	}
+	
+	
+
+}
